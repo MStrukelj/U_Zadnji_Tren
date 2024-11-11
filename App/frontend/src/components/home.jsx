997713cf7d42c74ccb9c66b8600482efddf3e644
AndreaA44 <andrea.andelkovic@fer.unizg.hr>
2024-11-12 00:39:35 +0100

    fixed image bug

--- App/frontend/src/components/home.jsx
+++ App/frontend/src/components/home.jsx
@@ -1,6 +1,7 @@
 // src/components/Home.jsx
 import React, { useState } from 'react';
 import './home.css';
+import SkolaImg from '../assets/skola.jpg';
 
 function Home() {
     const [sidebarVisible, setSidebarVisible] = useState(false);
@@ -10,7 +11,7 @@ function Home() {
     };
 
     return (
-        <div className="home-container">
+        <div className="container">
             {/* Header with menu button and school logo */}
             <header className="header">
                 <button className="menu-button" onClick={toggleSidebar}>
@@ -27,8 +28,8 @@ function Home() {
             {sidebarVisible && (
                 <aside className="sidebar">
                     <button className="sidebar-button">PREDMETI</button>
-                    <button className="sidebar-button">POTVRDE</button>
                     <button className="sidebar-button">KALENDAR</button>
+                    <button className="sidebar-button">POTVRDE</button>
                     <button className="sidebar-button">CHAT</button>
                 </aside>
             )}
@@ -36,7 +37,7 @@ function Home() {
             {/* Main Content */}
             <main className="content">
                 <section className="info-section">
-                    <img className="info-image" alt="Škola" src="../assets/skola.jpg"/>
+                    <img className="info-image" alt="Škola" src={SkolaImg}/>
                     <h2 className="info-title">NEŠTO O NAMA</h2>
                     <p className="info-text">
                         Diplomski studij. Diplomski studij organizira se kroz tri studijska programa,
