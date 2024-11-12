519f96123a9f4838804676b2479c5237d34850c1
AndreaA44 <andrea.andelkovic@fer.unizg.hr>
2024-11-12 23:37:55 +0100

    Debugged disruptive code + fixed layout

--- App/frontend/src/components/LogIn.jsx
+++ App/frontend/src/components/LogIn.jsx
@@ -13,23 +13,33 @@ function LogIn({FormHandle}) {
       
    }
    return (
-   <div className="form-container">
-   <h2>PRIJAVA</h2>
-   <form onSubmit={handleLogin}>
-   <div className="form-control">
-   <input type="text" placeholder="Enter your email" 
-      onChange={(e)=>setUser(e.target.value)} />
-   </div>
-   
-   <div className="form-control">
-   <input type="password" placeholder="Enter your password" 
-     onChange={(e)=>setPassword(e.target.value)}/>
-   </div>
-   
-   <button onClick={handleLogin}>PRIJAVA</button>
-   </form>
-   <p onClick={() => FormHandle("signup")}>Novi korsinik? Registriraj se</p>
-   </div>
+     <div className="auth-wrapper">
+       <div className="form-container">
+         <h2>PRIJAVA</h2>
+         <form onSubmit={handleLogin}>
+           <div className="form-control">
+             <input
+               type="text"
+               placeholder="Enter your email"
+               onChange={(e) => setUser(e.target.value)}
+             />
+           </div>
+
+           <div className="form-control">
+             <input
+               type="password"
+               placeholder="Enter your password"
+               onChange={(e) => setPassword(e.target.value)}
+             />
+           </div>
+
+           <button onClick={handleLogin}>PRIJAVA</button>
+         </form>
+         <p onClick={() => FormHandle("signup")}>
+           Novi korsinik? Registriraj se
+         </p>
+       </div>
+     </div>
    );
    }
    
--- App/frontend/src/components/SignUp.jsx
+++ App/frontend/src/components/SignUp.jsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import './login.css';
+import React, { useState } from "react";
+import "./login.css";
 
 function SignUp({ FormHandle }) {
   // State to keep track of selected options
@@ -7,122 +7,134 @@ function SignUp({ FormHandle }) {
   const [izborniPredmeti, setIzborniPredmeti] = useState([]);
 
   // Handler to manage changes in Smjer selection
-const handleSmjerChange = (event) => {
-  const { value, checked } = event.target;
-  if (checked) {
-    // Set smjer to an array containing only the selected value
-    setSmjer([value]);
-  } else {
-    // If unchecked, clear the smjer array
-    setSmjer([]);
-  }
-};
+  const handleSmjerChange = (event) => {
+    const { value, checked } = event.target;
+    if (checked) {
+      // Set smjer to an array containing only the selected value
+      setSmjer([value]);
+    } else {
+      // If unchecked, clear the smjer array
+      setSmjer([]);
+    }
+  };
 
-// Handler to manage changes in Izborni Predmeti selection
-const handleIzborniPredmetiChange = (event) => {
-  const { value, checked } = event.target;
-  if (checked) {
-    // Set izborniPredmeti to an array containing only the selected value
-    setIzborniPredmeti([value]);
-  } else {
-    // If unchecked, clear the izborniPredmeti array
-    setIzborniPredmeti([]);
-  }
-};
-
+  // Handler to manage changes in Izborni Predmeti selection
+  const handleIzborniPredmetiChange = (event) => {
+    const { value, checked } = event.target;
+    if (checked) {
+      // Set izborniPredmeti to an array containing only the selected value
+      setIzborniPredmeti([value]);
+    } else {
+      // If unchecked, clear the izborniPredmeti array
+      setIzborniPredmeti([]);
+    }
+  };
 
   return (
-    <div className="form-container">
-      <h2>REGISTRACIJA</h2>
-      <form>
-        <div className="form-control">
-          <input type="text" placeholder="Enter your email" />
-        </div>
-
-        <div className="form-control">
-          <input type="password" placeholder="Enter your password" />
-        </div>
-
-        {/* Smjer selection */}
-        <div className="form-control">
-          <label><b>Smjer:</b></label>
-          <div className="checkbox-group">
-          <div className="checkbox-container">
-          <label>
-            <input
-              type="checkbox"
-              value="Opci"
-              onChange={handleSmjerChange}
-              checked={smjer.includes("Opci")}
-            />
-            A-Opći</label>
+    <div className="auth-wrapper">
+      <div className="form-container">
+        <h2>REGISTRACIJA</h2>
+        <form>
+          <div className="form-control">
+            <input type="text" placeholder="Enter your email" />
           </div>
 
-          <div className="checkbox-container">
-          <label>
-            <input
-              type="checkbox"
-              value="Informaticki"
-              onChange={handleSmjerChange}
-              checked={smjer.includes("Informaticki")}
-            />
-            B-Informatički</label>
+          <div className="form-control">
+            <input type="password" placeholder="Enter your password" />
           </div>
 
-          <div className="checkbox-container">
-          <label>
-            <input
-              type="checkbox"
-              value="Matematicki"
-              onChange={handleSmjerChange}
-              checked={smjer.includes("Matematicki")}
-            />
-            C-Matematički</label>
-          </div>
-          </div>
-        </div>
+          {/* Smjer selection */}
+          <div className="form-control">
+            <label>
+              <b>Smjer:</b>
+            </label>
+            <div className="checkbox-group">
+              <div className="checkbox-container">
+                <label>
+                  <input
+                    type="checkbox"
+                    value="Opci"
+                    onChange={handleSmjerChange}
+                    checked={smjer.includes("Opci")}
+                  />
+                  A-Opći
+                </label>
+              </div>
 
-        {/* Izborni Predmeti selection */}
-        <div className="form-control">
-          <label><b>Izborni predmeti:</b></label>
-          <div className="checkbox-group">
-          <div className="checkbox-container">
-          <label>
-            <input
-              type="checkbox"
-              value="Francuski"
-              onChange={handleIzborniPredmetiChange}
-              checked={izborniPredmeti.includes("Francuski")}
-            />
-            Francuski</label>
-          </div>
-          <div className="checkbox-container">
-          <label>
-            <input
-              type="checkbox"
-              value="Njemacki"
-              onChange={handleIzborniPredmetiChange}
-              checked={izborniPredmeti.includes("Njemacki")}
-            />
-            Njemački-DSD</label>
-          </div>
-          <div className="checkbox-container">
-          <label>
-            <input
-              type="checkbox"
-              value="Astronomija"
-              onChange={handleIzborniPredmetiChange}
-              checked={izborniPredmeti.includes("Astronomija")}
-            />
-            Astronomija</label>
-          </div>
-          
-          </div>
-        </div>
+              <div className="checkbox-container">
+                <label>
+                  <input
+                    type="checkbox"
+                    value="Informaticki"
+                    onChange={handleSmjerChange}
+                    checked={smjer.includes("Informaticki")}
+                  />
+                  B-Informatički
+                </label>
+              </div>
 
-        <button>REGISTRACIJA</button>
-      </form>
-      <p onClick={() => FormHandle("login")}>Already have an account? Login</p>
+              <div className="checkbox-container">
+                <label>
+                  <input
+                    type="checkbox"
+                    value="Matematicki"
+                    onChange={handleSmjerChange}
+                    checked={smjer.includes("Matematicki")}
+                  />
+                  C-Matematički
+                </label>
+              </div>
+            </div>
+          </div>
+
+          {/* Izborni Predmeti selection */}
+          <div className="form-control">
+            <label>
+              <b>Izborni predmeti:</b>
+            </label>
+            <div className="checkbox-group">
+              <div className="checkbox-container">
+                <label>
+                  <input
+                    type="checkbox"
+                    value="Francuski"
+                    onChange={handleIzborniPredmetiChange}
+                    checked={izborniPredmeti.includes("Francuski")}
+                  />
+                  Francuski
+                </label>
+              </div>
+              <div className="checkbox-container">
+                <label>
+                  <input
+                    type="checkbox"
+                    value="Njemacki"
+                    onChange={handleIzborniPredmetiChange}
+                    checked={izborniPredmeti.includes("Njemacki")}
+                  />
+                  Njemački-DSD
+                </label>
+              </div>
+              <div className="checkbox-container">
+                <label>
+                  <input
+                    type="checkbox"
+                    value="Astronomija"
+                    onChange={handleIzborniPredmetiChange}
+                    checked={izborniPredmeti.includes("Astronomija")}
+                  />
+                  Astronomija
+                </label>
+              </div>
+            </div>
+          </div>
+
+          <button>REGISTRACIJA</button>
+        </form>
+        <p onClick={() => FormHandle("login")}>
+          Already have an account? Login
+        </p>
+      </div>
     </div>
   );
 }
