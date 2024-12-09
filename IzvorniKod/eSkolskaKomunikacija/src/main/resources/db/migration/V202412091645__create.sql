03a5711ab465e731fa9823bcf71c19bb2f47c95e
Lockdown4 <127786815+Lockdown4@users.noreply.github.com>
2024-12-09 18:52:04 +0100

    DB refactor

--- IzvorniKod/eSkolskaKomunikacija/src/main/resources/db/migration/V202412091645__create.sql
+++ IzvorniKod/eSkolskaKomunikacija/src/main/resources/db/migration/V202412091645__create.sql
@@ -7,14 +7,14 @@ CREATE TABLE MJESTO
 
 CREATE TABLE RAZRED
 (
-    oznRaz varchar(10) NOT NULL,
+    oznRaz VARCHAR(10) NOT NULL,
     smjer VARCHAR(1),
     godina INT,
     kapacitet INT NOT NULL,
     izboran VARCHAR(20),
     fakultativan VARCHAR(20),
     PRIMARY KEY (oznRaz),
-    CONSTRAINT checkoznRaz CHECK(CHAR_LENGTH(oznRaz) < 10)
+    CONSTRAINT checkoznRaz CHECK (CHAR_LENGTH(oznRaz) < 10)
 );
 
 CREATE TABLE PREDMET
@@ -25,10 +25,10 @@ CREATE TABLE PREDMET
     izboran BOOLEAN NOT NULL,
     fakultativan BOOLEAN NOT NULL,
     PRIMARY KEY (sifPredmet),
-    UNIQUE(nazPred)
+    UNIQUE (nazPred)
 );
 
-CREATE UNIQUE INDEX idx_nazPred ON PREDMET(nazPred);
+CREATE UNIQUE INDEX idx_nazPred ON PREDMET (nazPred);
 
 CREATE TABLE UCIONICA
 (
@@ -45,9 +45,9 @@ CREATE TABLE KORISNIK
     prezime VARCHAR NOT NULL,
     uloga1 VARCHAR(6) NOT NULL,
     uloga2 VARCHAR(6),
-    PRIMARY KEY (email, lozinka),
-    CONSTRAINT checkUloga1 CHECK(CHAR_LENGTH(uloga1)<=6),
-    CONSTRAINT checkUloga2 CHECK(CHAR_LENGTH(uloga2)<=6)
+    PRIMARY KEY (email),
+    CONSTRAINT checkUloga1 CHECK (CHAR_LENGTH(uloga1) <= 6),
+    CONSTRAINT checkUloga2 CHECK (CHAR_LENGTH(uloga2) <= 6)
 );
 
 CREATE TABLE UCENIK
@@ -58,46 +58,44 @@ CREATE TABLE UCENIK
     pbrStan INT NOT NULL,
     pbrRod INT NOT NULL,
     email VARCHAR NOT NULL,
-    lozinka VARCHAR NOT NULL,
     PRIMARY KEY (JMBAG),
     FOREIGN KEY (pbrStan) REFERENCES MJESTO(pbr),
     FOREIGN KEY (pbrRod) REFERENCES MJESTO(pbr),
-    FOREIGN KEY (email, lozinka) REFERENCES KORISNIK(email, lozinka),
+    FOREIGN KEY (email) REFERENCES KORISNIK(email),
     UNIQUE (OIB),
-    UNIQUE (email, lozinka),
-    CONSTRAINT checkOIB CHECK(CHAR_LENGTH(OIB) = 11)
+    UNIQUE (email),
+    CONSTRAINT checkOIB CHECK (CHAR_LENGTH(OIB) = 11)
 );
 
 CREATE TABLE NASTAVNIK
 (
     sifNast INT NOT NULL,
     email VARCHAR NOT NULL,
-    lozinka VARCHAR NOT NULL,
     PRIMARY KEY (sifNast),
-    FOREIGN KEY (email, lozinka) REFERENCES KORISNIK(email, lozinka),
-    UNIQUE (email, lozinka)
+    FOREIGN KEY (email) REFERENCES KORISNIK(email),
+    UNIQUE (email)
 );
 
 CREATE TABLE PREDMETRAZRED
 (
-    oznRaz varchar(10) NOT NULL,
+    oznRaz VARCHAR(10) NOT NULL,
     sifPredmet INT NOT NULL,
     sifNast INT NOT NULL,
     PRIMARY KEY (oznRaz, sifPredmet),
     FOREIGN KEY (oznRaz) REFERENCES RAZRED(oznRaz),
     FOREIGN KEY (sifPredmet) REFERENCES PREDMET(sifPredmet),
     FOREIGN KEY (sifNast) REFERENCES NASTAVNIK(sifNast),
-    CONSTRAINT checkoznRaz CHECK(CHAR_LENGTH(oznRaz) < 10)
+    CONSTRAINT checkoznRaz CHECK (CHAR_LENGTH(oznRaz) < 10)
 );
 
 CREATE TABLE upisao
 (
-    oznRaz varchar(10) NOT NULL,
+    oznRaz VARCHAR(10) NOT NULL,
     JMBAG INT NOT NULL,
     PRIMARY KEY (oznRaz, JMBAG),
     FOREIGN KEY (oznRaz) REFERENCES RAZRED(oznRaz),
     FOREIGN KEY (JMBAG) REFERENCES UCENIK(JMBAG),
-    CONSTRAINT checkoznRaz CHECK(CHAR_LENGTH(oznRaz) < 10)
+    CONSTRAINT checkoznRaz CHECK (CHAR_LENGTH(oznRaz) < 10)
 );
 
 CREATE TABLE kompatibilan
@@ -114,7 +112,7 @@ CREATE TABLE PREDAVANJE
     datVrijemePoc TIMESTAMP NOT NULL,
     datVrijemeKraj TIMESTAMP NOT NULL,
     oznUcionica VARCHAR NOT NULL,
-    oznRaz varchar(3) NOT NULL,
+    oznRaz VARCHAR(3) NOT NULL,
     sifPredmet INT NOT NULL,
     PRIMARY KEY (datVrijemePoc, oznRaz, sifPredmet),
     FOREIGN KEY (oznUcionica) REFERENCES UCIONICA(oznUcionica),
