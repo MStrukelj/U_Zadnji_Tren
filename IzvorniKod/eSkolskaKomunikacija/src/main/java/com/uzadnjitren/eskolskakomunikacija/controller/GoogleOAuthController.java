e6301cfb79a57f49eb624c2d1f1b4ba0219966b0
Lockdown4 <127786815+Lockdown4@users.noreply.github.com>
2024-12-09 15:48:14 +0100

    Dodan import

--- IzvorniKod/eSkolskaKomunikacija/src/main/java/com/uzadnjitren/eskolskakomunikacija/controller/GoogleOAuthController.java
+++ IzvorniKod/eSkolskaKomunikacija/src/main/java/com/uzadnjitren/eskolskakomunikacija/controller/GoogleOAuthController.java
@@ -1,13 +1,16 @@
 package com.uzadnjitren.eskolskakomunikacija.controller;
 
-import org.springframework.web.bind.annotation.*;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+
 import java.util.HashMap;
 import java.util.Map;
+
 
 @RestController
 @RequestMapping("/api/oauth")
